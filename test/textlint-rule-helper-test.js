44db06f683ac9b735272e5c5e4c5b18ba2abfda3
azu <azuciao@gmail.com>
2015-01-08 16:27:16 +0900

    feat(helper): implement `#getParents` and `isChildNode`

--- test/textlint-rule-helper-test.js
+++ test/textlint-rule-helper-test.js
@@ -3,11 +3,88 @@
 import support from 'source-map-support'
 support.install();
 
+import {parse,Syntax} from "markdown-to-ast"
+import {traverse} from "txt-ast-traverse"
 import assert from 'power-assert'
 import {RuleHelper} from "../lib/textlint-rule-helper.js";
+import {textlint} from "textlint"
 describe("textlint-rule-helper-test", function () {
-    it("test", ()=> {
-        var helper = new RuleHelper();
-        assert.equal(helper.add(1, 2), 1);
+    afterEach(function () {
+        textlint.resetRules();
+    });
+    describe("#getParents()", ()=> {
+        context("on Document", ()=> {
+            it("should return []", () => {
+                var text = "# Header";
+                var parents = [];
+                textlint.setupRules({
+                    "rule-key": function (context) {
+                        var helper = new RuleHelper(context);
+                        return {
+                            [context.Syntax.Document](node) {
+                                parents = helper.getParents(node)
+                            }
+                        }
+                    }
+                });
+                textlint.lintMarkdown(text);
+                assert(parents.length === 0);
+            });
+
+        });
+        it("should return parents", () => {
+            var text = "# Header";
+            var parents = [];
+            textlint.setupRules({
+                "rule-key": function (context) {
+                    var helper = new RuleHelper(context);
+                    return {
+                        [context.Syntax.Str](node) {
+                            parents = helper.getParents(node)
+                        }
+                    }
+                }
+            });
+            textlint.lintMarkdown(text);
+            assert(parents.length === 2);
+        });
+    });
+    describe("#isChildNode(node, parentTypes)", ()=> {
+        context("when the parent node is Paragraph, the child node is Str", ()=> {
+            it("should be true", () => {
+                var text = "text";
+                var result;
+                textlint.setupRules({
+                    "rule-key": function (context) {
+                        var helper = new RuleHelper(context);
+                        return {
+                            [context.Syntax.Str](node) {
+                                result = helper.isChildNode(node, [context.Syntax.Paragraph])
+                            }
+                        }
+                    }
+                });
+                textlint.lintMarkdown(text);
+                assert.ok(result);
+            });
+        });
+        context("when the parent node is Str, the child node is Paragraph", ()=> {
+            it("should be false", () => {
+                var text = "text";
+                var result;
+                textlint.setupRules({
+                    "rule-key": function (context) {
+                        var helper = new RuleHelper(context);
+                        return {
+                            [context.Syntax.Paragraph](node) {
+                                result = helper.isChildNode(node, [context.Syntax.Str])
+                            }
+                        }
+                    }
+                });
+                textlint.lintMarkdown(text);
+                assert.ok(!result);
+            });
+        });
     });
 });
