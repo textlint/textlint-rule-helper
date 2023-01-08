d368ec35dbec9fde4ec589fe3cad34c018ad35e5
azu <azuciao@gmail.com>
2023-01-08 20:24:40 +0900

    refactor: update structured-source and use @textlint/kernel

--- test/wrap-report-handler-test.ts
+++ test/wrap-report-handler-test.ts
@@ -1,39 +1,40 @@
 // LICENSE : MIT
 import assert from 'assert'
-import { textlint } from "textlint"
-import Source from "structured-source"
-import { wrapReportHandler } from "../src/wrap-report-handler";
-import { TextlintRuleReporter } from "@textlint/types";
+import { TextlintKernel, TextlintRuleModule } from "@textlint/kernel"
+import markdown from "@textlint/textlint-plugin-markdown"
+import { StructuredSource } from "structured-source"
+import { wrapReportHandler } from "../src";
 import { AnyTxtNode, ASTNodeTypes } from "@textlint/ast-node-types";
 
+const builtInPlugins = [{
+    pluginId: "markdown",
+    plugin: markdown
+}];
+
 describe("wrapReportHandler", function () {
-    afterEach(function () {
-        textlint.resetRules();
-    });
     describe("ignoreNodeTypes", () => {
         it("should just ignore node", () => {
-            textlint.setupRules({
-                "rule-key": function (context) {
-                    const { RuleError } = context;
-                    return wrapReportHandler(context, {
-                        ignoreNodeTypes: Object.keys(ASTNodeTypes).concat("my-type")
-                    }, report => {
-                        return {
-                            ...(Object.keys(ASTNodeTypes).reduce((object, key) => {
-                                object[key] = (node: AnyTxtNode) => {
-                                    report(node, new RuleError(node.type));
-                                };
-                                return object;
-                            }, {} as any)),
-                            ...{
-                                "my-type"(node) {
-                                    report(node, new RuleError(node.type));
-                                }
+            const textlint = new TextlintKernel()
+            const rule: TextlintRuleModule = function (context) {
+                const { RuleError } = context;
+                return wrapReportHandler(context, {
+                    ignoreNodeTypes: Object.keys(ASTNodeTypes).concat("my-type")
+                }, report => {
+                    return {
+                        ...(Object.keys(ASTNodeTypes).reduce((object, key) => {
+                            object[key] = (node: AnyTxtNode) => {
+                                report(node, new RuleError(node.type));
+                            };
+                            return object;
+                        }, {} as any)),
+                        ...{
+                            "my-type"(node) {
+                                report(node, new RuleError(node.type));
                             }
-                        };
-                    });
-                } as TextlintRuleReporter
-            });
+                        }
+                    };
+                });
+            }
             const text = `# Header
 
 **This** *is* \`code\`.
@@ -57,31 +58,46 @@ code
 [1]: "1"
 
 `;
-            return textlint.lintText(text, ".md").then((result) => {
+            return textlint.lintText(text, {
+                ext: ".md",
+                rules: [
+                    {
+                        ruleId: "test",
+                        rule: rule,
+                    }
+                ],
+                plugins: builtInPlugins
+            }).then((result) => {
                 assert.strictEqual(result.messages.length, 0, "Should ignore all nodes");
             });
         });
         it("should ignore parent node by types", () => {
             let isCalled = false;
-            textlint.setupRules({
-                "rule-key": function (context: any) {
-                    return wrapReportHandler(context, {
-                        ignoreNodeTypes: [context.Syntax.BlockQuote, context.Syntax.Code]
-                    }, _report => {
-                        return {
-                            [context.Syntax.Paragraph]() {
-                                isCalled = true
-                            },
-                            [context.Syntax.Str]() {
-                                isCalled = true
-                            }
+            const textlint = new TextlintKernel()
+            const rule: TextlintRuleModule = function (context: any) {
+                return wrapReportHandler(context, {
+                    ignoreNodeTypes: [context.Syntax.BlockQuote, context.Syntax.Code]
+                }, _report => {
+                    return {
+                        [context.Syntax.Paragraph]() {
+                            isCalled = true
+                        },
+                        [context.Syntax.Str]() {
+                            isCalled = true
                         }
-                    });
-                }
-            });
+                    }
+                });
+            }
             const text = `> This should be ignored.`;
-            return textlint.lintText(text, ".md").then(() => {
-                assert.ok(isCalled === false, "Str node handler should not be called");
+            return textlint.lintText(text, {
+                ext: ".md",
+                rules: [{
+                    ruleId: "test",
+                    rule
+                }],
+                plugins: builtInPlugins
+            }).then(() => {
+                assert.strictEqual(isCalled, false, "Str node handler should not be called");
             });
         });
         it("should ignore child node by types", () => {
@@ -123,66 +139,78 @@ code
                     ignored: false
                 },
             ];
-            textlint.setupRules({
-                "rule-key": function (context) {
-                    const { Syntax, getSource } = context;
-                    return wrapReportHandler(context, {
-                        ignoreNodeTypes: [context.Syntax.Code]
-                    }, report => {
-                        return {
-                            [Syntax.Paragraph](node) {
-                                const text = getSource(node);
-                                expectedList.forEach(item => {
-                                    const index = text.search(item.name);
-                                    report(node, new context.RuleError(item.name, {
-                                        index
-                                    }))
-                                });
-                            }
+            const textlint = new TextlintKernel();
+            const rule: TextlintRuleModule = function (context) {
+                const { Syntax, getSource } = context;
+                return wrapReportHandler(context, {
+                    ignoreNodeTypes: [context.Syntax.Code]
+                }, report => {
+                    return {
+                        [Syntax.Paragraph](node) {
+                            const text = getSource(node);
+                            expectedList.forEach(item => {
+                                const index = text.search(item.name);
+                                report(node, new context.RuleError(item.name, {
+                                    index
+                                }))
+                            });
                         }
-                    });
-                } as TextlintRuleReporter
-            });
+                    }
+                });
+            };
             const text = "123`456`789";
-            return textlint.lintMarkdown(text).then((result) => {
+            return textlint.lintText(text, {
+                ext: ".md",
+                rules: [{
+                    ruleId: "test",
+                    rule
+                }],
+                plugins: builtInPlugins
+            }).then((result) => {
                 const messages = result.messages;
                 messages.forEach(message => {
                     const matchItem = expectedList.find(item => item.name === message.message);
                     if (!matchItem) {
                         throw new Error(`Not found match item:${message}`);
                     }
-                    assert.ok(matchItem.ignored === false, `Should ignore item: ${JSON.stringify(matchItem)}`);
+                    assert.strictEqual(matchItem.ignored, false, `Should ignore item: ${JSON.stringify(matchItem)}`);
                 })
             });
         });
         it("should support line,column nodes", () => {
             const text = "> Line 1\n"
                 + "> This is `code`.";
-            textlint.setupRules({
-                "rule-key": function (context) {
-                    const { Syntax, RuleError } = context;
-                    return wrapReportHandler(context, {
-                        ignoreNodeTypes: [context.Syntax.Code]
-                    }, report => {
-                        return {
-                            [Syntax.Paragraph](node) {
-                                const text = context.getSource(node);
-                                const source = new Source(text);
-                                const indexOfCode = text.search("code");
-                                if (indexOfCode === -1) {
-                                    return;
-                                }
-                                const loc = source.indexToPosition(indexOfCode);
-                                report(node, new RuleError(text, {
-                                    line: loc.line - 1,
-                                    column: loc.column
-                                }));
+            const textlint = new TextlintKernel();
+            const rule: TextlintRuleModule = function (context) {
+                const { Syntax, RuleError } = context;
+                return wrapReportHandler(context, {
+                    ignoreNodeTypes: [context.Syntax.Code]
+                }, report => {
+                    return {
+                        [Syntax.Paragraph](node) {
+                            const text = context.getSource(node);
+                            const source = new StructuredSource(text);
+                            const indexOfCode = text.search("code");
+                            if (indexOfCode === -1) {
+                                return;
                             }
+                            const loc = source.indexToPosition(indexOfCode);
+                            report(node, new RuleError(text, {
+                                line: loc.line - 1,
+                                column: loc.column
+                            }));
                         }
-                    });
-                } as TextlintRuleReporter
-            });
-            return textlint.lintMarkdown(text).then((result) => {
+                    }
+                });
+            }
+            return textlint.lintText(text, {
+                ext: ".md",
+                rules: [{
+                    ruleId: "test",
+                    rule
+                }],
+                plugins: builtInPlugins
+            }).then((result) => {
                 const messages = result.messages;
                 assert.strictEqual(messages.length, 0);
             });
